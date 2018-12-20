76d3a09eb4cc682e902a1d7d9c35f0e2ad1224e5
bsezgun <bsezgun@hotmail.com>
2018-12-20 20:43:07 +0300

    Create Car Entity to park in slots

---
+++ src/main/java/com/hexad/parkinglot/entity/Car.java
@@ -0,0 +1,38 @@
+package com.hexad.parkinglot.entity;
+
+import java.math.BigDecimal;
+
+public class Car {
+
+	private BigDecimal slotNumber;
+	private String colour;
+	private String registrationNumber;
+	
+	public Car(BigDecimal slotNumber,  String registrationNumber,String colour) {
+		super();
+		this.slotNumber = slotNumber;
+		this.colour = colour;
+		this.registrationNumber = registrationNumber;
+	}
+	
+	public BigDecimal getSlotNumber() {
+		return slotNumber;
+	}
+	public void setSlotNumber(BigDecimal slotNumber) {
+		this.slotNumber = slotNumber;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+	
+	
+}
