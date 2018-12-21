34131cb6e966fabdfe0993360ad70a38c83df900
bsezgun <bsezgun@hotmail.com>
2018-12-21 19:53:59 +0300

    Add extra attributes to control at the test cases

--- src/main/java/com/hexad/parkinglot/entity/ResultPark.java
+++ src/main/java/com/hexad/parkinglot/entity/ResultPark.java
@@ -1,19 +1,28 @@
 package com.hexad.parkinglot.entity;
 
-import java.util.Optional;
+import java.util.List;
 
 public class ResultPark {
 
 	private String status;
 	private Car car;
+	private List<String> carAttributes;
 	
-	
-	
-	public ResultPark(String status, Optional<Car> car) {
+	public ResultPark(String status) {
 		this.status = status;
-		this.car = car.orElse(null);
 	}
 	
+	public ResultPark(String status, Car car) {
+		this.status = status;
+		this.car = car;
+	}
+
+	
+	public ResultPark(String status, List<String> carAttributes) {
+		this.status = status;
+		this.carAttributes=carAttributes;
+	}
+
 	public String getStatus() {
 		return status;
 	}
@@ -25,6 +34,14 @@ public class ResultPark {
 	}
 	public void setCar(Car car) {
 		this.car = car;
+	}
+
+	public List<String> getCarAttributes() {
+		return carAttributes;
+	}
+
+	public void setCarAttributes(List<String> carAttributes) {
+		this.carAttributes = carAttributes;
 	}
 	
 	
